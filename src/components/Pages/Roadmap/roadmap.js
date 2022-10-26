984b7954482340bf9063288e300ae162f150a415
bakthav <35214758+bakthav@users.noreply.github.com>
2022-10-26 20:08:13 +0530

    Add files via upload

--- src/components/Pages/Roadmap/roadmap.js
+++ src/components/Pages/Roadmap/roadmap.js
@@ -1,17 +1,4 @@
 import React from 'react';
-// import marbleBg from '../../../assets/images/roadmap-bg.jpg';
-// import icons1 from '../../../assets/images/marble-icons/1.png';
-// import icons2 from '../../../assets/images/marble-icons/2.png';
-// import icons3 from '../../../assets/images/marble-icons/3.png';
-// import icons4 from '../../../assets/images/marble-icons/4.png';
-// import icons5 from '../../../assets/images/marble-icons/5.png';
-// import icons6 from '../../../assets/images/marble-icons/6.png';
-// import icons7 from '../../../assets/images/marble-icons/7.png';
-// import icons8 from '../../../assets/images/marble-icons/8.png';
-// import icons9 from '../../../assets/images/marble-icons/9.png';
-// import icons10 from '../../../assets/images/marble-icons/10.png';
-// import icons11 from '../../../assets/images/marble-icons/11.png';
-// import icons12 from '../../../assets/images/marble-icons/12.png';
 
 const Roadmap = () => {
     (function () {
@@ -50,7 +37,7 @@ const Roadmap = () => {
           <h2>ROADMAP</h2>
       </div>
       <div className='text-center title-sub-txt mb-4'>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rutrum leo eget ligula vestibulum, ac auctor tortor vulputate. 
+      Long term and commited to building a powerful NFT powered platform for World Cup fans until the next one in 2026!
     </div>
     <div className='roadmap'>
     <div className='container'>     
@@ -65,31 +52,31 @@ const Roadmap = () => {
 <section class="br"></section>
 </div1>
 <section class="info no-border">
-<h2>Q1</h2>
+<h2></h2>
 <ul className='list'>
-    <li>Private sale</li>
-    <li>IDO</li>
-    <li>Origin series IGO</li>
-    <li>NFT Marketplace</li>
+    <li>Fair Launch</li>
+    <li>1st NFT collection</li>
+    <li>AI Matches v1</li>
+    <li>Betting contracts</li>
 </ul>
 </section>
 <section></section>
 
 <section></section>
 <section class="info">
-<h2>Q2</h2>
+<h2></h2>
 <ul className='list-2'>
-    <li>Private sale</li>
-    <li>IDO</li>
-    <li>Origin series IGO</li>
+    <li>Countries collection</li>
+    <li>AI Matches v2</li>
     <li>NFT Marketplace</li>
+    <li>Betting contracts 2.0</li>
 </ul>
 </section>
 <div1 class="circle-container">
 <section class="tl"></section>
 <section class="tr"></section>
 <div class="flex">
-  <span class="circle">2022</span>
+  <span class="circle">2023</span>
 </div>
 <section class="bl"></section>
 <section class="br"></section>
@@ -98,37 +85,38 @@ const Roadmap = () => {
 <section class="tl"></section>
 <section class="tr"></section>
 <div class="flex">
-  <span class="circle">2022</span>
+  <span class="circle">2024</span>
 </div>
 <section class="bl"></section>
 <section class="br"></section>
 </div1>
 <section class="info">
-<h2>Q3</h2>
+<h2></h2>
 <ul className='list'>
-    <li>Private sale</li>
-    <li>IDO</li>
-    <li>Origin series IGO</li>
-    <li>NFT Marketplace</li>
+    <li>Create Teams</li>
+    <li>Custom Matches</li>
+    <li>Visual game v1</li>
+    <li>Virtual commentators</li>
 </ul>
 </section>
 <section></section>
 
+
 <section></section>
 <section class="info">
-<h2>Q4</h2>
+<h2></h2>
 <ul className='list-2'>
-    <li>Private sale</li>
-    <li>IDO</li>
-    <li>Origin series IGO</li>
-    <li>NFT Marketplace</li>
+    <li>Tourneys with $ prizes</li>
+    <li>AI Matches v3</li>
+    <li>Visual games v2</li>
+    <li>NFT proteges</li>
 </ul>
 </section>
 <div1 class="circle-container">
 <section class="tl"></section>
 <section class="tr"></section>
 <div class="flex">
-  <span class="circle">2022</span>
+  <span class="circle">2025</span>
 </div>
 <section class="bl no-border"></section>
 <section class="br no-border"></section>
