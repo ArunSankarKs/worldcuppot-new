8b3ff0ce96856962be5faceaf98b9b0e3d9c09dd
bakthav <35214758+bakthav@users.noreply.github.com>
2022-10-26 20:05:59 +0530

    Add files via upload

--- src/components/Pages/About/about.js
+++ src/components/Pages/About/about.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import game12 from '../../../assets/images/game12.jpg';
-import stadium from '../../../assets/images/stadium.png';
+import game12 from '../../../assets/images/game12.png';
+import blinkimg from '../../../assets/images/blink-img.png';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
@@ -26,10 +26,10 @@ const About = () => {
        }>
         <div className='black-wrapper'>
         <div className="text-right d-flex justify-content-center pt-5 pb-5">
-          <img src={game12}  className="img-fluid" data-inviewport="slide-up-particle" />
+          <img src={game12}  className="img-fluid"/>
         </div>
           <div className="text-right d-flex justify-content-center mt-5">
-          <img src={stadium}  className="img-fluid" data-inviewport="slide-left" />
+          <img src={blinkimg}  className="img-fluid"/>
           </div>
           <div className='text-animation-wrap'>
             <p>Card 975 from team A in Inside left Runs Around the ball</p>
@@ -38,14 +38,14 @@ const About = () => {
             <p>LCard 975 from team A in Center half back Injures to Card 432</p>          
           </div>
           <div className='title-style title-style-sm pb-1 mb-4 text-center mt-5'>
-            <h2>BETTING STATS: $550 BUSD</h2>
+            <h2>POT: $40 USDT</h2>
         </div>
-          <div className='table-responsive betting-table px--lg-5 mb-5'>
+          <div className='table-responsive betting-table px-5 mb-5'>
           <table class="table table-hover table-dark table-lg">
             <thead>
               <tr>
-                <th scope="col">TEAM BULL: $320</th>
-                <th scope="col" className='text-center'>TEAM BEAR: $230</th>
+                <th scope="col">TEAM BULL: $160</th>
+                <th scope="col" className='text-center'>TEAM BEAR: $180</th>
               </tr>
             </thead>
             <tbody>
